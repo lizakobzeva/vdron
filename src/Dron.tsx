271b39b18e8e78f090d6e08ca87577ba744a064f
lizakobzeva <lizakobzeva@list.ru>
2025-05-29 11:55:55 +0300

    first commit

---
+++ src/Dron.tsx
@@ -0,0 +1,30 @@
+import {Canvas} from "@react-three/fiber";
+import {useLoader} from '@react-three/fiber'
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js'
+import {PresentationControls, Stage} from "@react-three/drei";
+import {Suspense} from "react";
+
+
+function Dron() {
+    const gltf = useLoader(GLTFLoader, '../public/models/dron/scene.gltf')
+    return (
+        <Canvas style={{width: '100%', height: '70vh'}}>
+            <fog attach="fog" args={["#2F4F4F", 10, 20]} />
+            <PresentationControls speed={2} global>
+                <Stage
+                    shadows={false}
+                    environment="city"
+                    intensity={0.3}
+                    castShadow={false}
+                >
+                    <Suspense fallback={null}>
+                        <primitive object={gltf.scene}/>
+                    </Suspense>
+                </Stage>
+            </PresentationControls>
+
+        </Canvas>
+    );
+}
+
+export default Dron;
